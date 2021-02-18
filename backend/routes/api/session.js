021ed8fa7348e2ed3a707b6b059d0ff45115c179
laura button <llalla02@msn.com>
2021-02-18 15:00:57 -0700

    logout route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -26,6 +26,17 @@ router.post(
         return res.json({
             user
         })
-    }))
+    })
+)
+
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token')
+        return res.json({ message: 'success' })
+    }
+)
+
+
 
 module.exports = router

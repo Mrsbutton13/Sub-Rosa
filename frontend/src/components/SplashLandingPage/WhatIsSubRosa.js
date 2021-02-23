a45c77554568faa485e56b680ec842ee505b855d
laura button <llalla02@msn.com>
2021-02-23 10:45:59 -0700

    splashpage2 done

---
+++ frontend/src/components/SplashLandingPage/WhatIsSubRosa.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Animator, ScrollContainer, ScrollPage} from 'react-scroll-motion'
+import './SplashLanding.css'
+
+function WhatIsSubRosa () {
+    return (
+        <>
+          <div className='splash-div2'>
+            <h1 className='what-is'>What is Sub-Rosa?</h1>
+            <div className='symbols'>
+              <span className='book'>
+                <i id='book-dead' className="fas fa-book-dead"></i>
+              </span>
+              <span className='crossbones'>
+                <i id='crossbones' className="fas fa-skull-crossbones"></i>
+              </span>
+              <span className='moon'>
+                <i id='moon' className="fas fa-cloud-moon"></i>
+              </span>
+            </div>
+            <div className='symbols2'>
+              <span className='crow'>
+                <i id='crow' className="fas fa-crow"></i>
+              </span>
+              <span className='ghost'>
+                <i id='ghost' className="fas fa-ghost"></i>
+              </span>   
+            </div> 
+            <div className='description-div'>
+            <h2 className='wanting-more' >Sub-Rosa will leave you wanting more.</h2>
+            <span className='description'>
+              Who doesn't love a scary, creepy story? Some might think they are true some might not.
+              Here you can write you're story, like many others who believe in the supernatural. Read, write, view photos, or videos 
+              of the supernatural. 
+            </span>        
+            </div>
+          </div> 
+        </>
+    )
+}
+
+export default WhatIsSubRosa
--- frontend/src/components/SplashLandingPage/index.js
+++ frontend/src/components/SplashLandingPage/index.js
@@ -4,10 +4,14 @@ import { NavLink, Redirect } from "react-router-dom"
 import LoginFormModal from "../LoginFormModal"
 import SignupFormModal from "../SignupFromModal"
 import './SplashLanding.css'
+import {Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn} from 'react-scroll-motion'
+import WhatIsSubRosa from './WhatIsSubRosa'
 
 
 function SplashPage({isLoaded} ) {
     const sessionUser = useSelector(state => state.session.user)
+    const ZoomInScrollOut= batch(StickyIn(), FadeIn(), ZoomIn())
+    const FadeUp = batch(Fade(), Move(), Sticky())
 
     let login 
     let createA
@@ -20,26 +24,36 @@ function SplashPage({isLoaded} ) {
     
     return (
         <>
-            <div className='splash-container'>
+          <ScrollContainer>
+            <ScrollPage page={0}>
+              <Animator animation={FadeUp}>
+              <div className='splash-container'>
                 <div className='splash-div1'>
-                    <h1 className='Sub-Rosa'>Sub-Rosa</h1>
-                    <section className='splash-section'>
-                        <div className='note'>
-                            Read other's experiences, look at things, talk about experiences.
-                            Enter the realm of the supernatural. 
-                        </div>
-                        <div className='splash-sub-div'>
-                        <div className='createA'>
-                            {isLoaded && createA}
-                        </div>
-                        <div className='login'>
-                            {isLoaded && login}
-                        </div>
-                        </div>
-                    </section>
+                  <h1 className='Sub-Rosa'>Sub-Rosa</h1>
+                  <section className='splash-section'>
+                    <div className='note'>
+                      Read other's experiences, look at things, talk about experiences.
+                      Enter the realm of the supernatural. 
+                    </div>
+                    <div className='splash-sub-div'>
+                      <div className='createA'>{isLoaded && createA}</div>
+                      <div className='login'>{isLoaded && login}</div>
+                    </div>
+                  </section>
                 </div>
-            </div>
-    
+              </div>
+              </Animator>
+            </ScrollPage>
+            <ScrollPage page={1}>
+              <Animator animation={ZoomInScrollOut}>
+                <WhatIsSubRosa/>
+              </Animator>
+            </ScrollPage>
+              <div className='splash-div3'>
+              </div>
+              <div className='splash-div4'>
+              </div>
+          </ScrollContainer>
         </>
     )
 }

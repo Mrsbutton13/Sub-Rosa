8a4043b8ec00819c252af23af418d7268454d73d
laura button <llalla02@msn.com>
2021-02-24 10:37:13 -0700

    database1

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const { Dashboard, Blog, Post, Comment, Follow, User} = require('../db/models')
-const asyncHandler = require('express-async-handler')
-const router = express.Router()
+// const express = require('express')
+// const { Dashboard, Blog, Post, Comment, Follow, User} = require('../db/models')
+// const asyncHandler = require('express-async-handler')
+// const router = express.Router()
 
-router.get('/', asyncHandler(async (req, res) => {
-    const myDashboard 
-}))
+// router.get('/', asyncHandler(async (req, res) => {
+//     const myDashboard 
+// }))

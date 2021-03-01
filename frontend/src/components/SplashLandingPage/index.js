896b664b87eff52cc9378d189b83de518481a166
laura button <llalla02@msn.com>
2021-02-28 17:10:22 -0700

    done

--- frontend/src/components/SplashLandingPage/index.js
+++ frontend/src/components/SplashLandingPage/index.js
@@ -29,8 +29,10 @@ function SplashPage({isLoaded}) {
                   <h1 className='Sub-Rosa'>Sub-Rosa</h1>
                   <section className='splash-section'>
                     <div className='note'>
-                      Read other's experiences, look at things, talk about experiences.
-                      Enter the realm of the supernatural. 
+                      Do you believe in ghost? Do you belive in aliens? Do you belive in big foot?
+                      Have you seen something unexplainable and never told anyone? Maybe you feel like 
+                      no one will believe you. Well fear no more, here you can write about your experiences incognito.
+                       Enter the realm of the supernatural. 
                     </div>
                     <div className='splash-sub-div'>
                       <div className='createA'>{isLoaded && createA}</div>
